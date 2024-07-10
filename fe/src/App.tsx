34e55e63e3cd3c6c9f7415408a507d5579eb5a85
eunseo <jangeun4097@naver.com>
2024-07-10 14:51:25 +0900

    Revert "로그인 ui 사용"
    This reverts commit bdbfc7a7dec0028ed5c1e8ce752310062a30463e.

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,17 +1,9 @@
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import LoginPage from "./domains/login/LoginPage";
-
-const theme = extendTheme({
-  fonts: {
-    body: "Inter, sans-serif", // 전역 body 요소에 적용될 글꼴
-  },
-});
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <LoginPage />
-    </ChakraProvider>
+    <div className="App">
+     fdf
+    </div>
   );
 }
 
